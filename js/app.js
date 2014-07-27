916d1b4f11888df906dd21dd14ae8b9df80ca3ad
Jennifer Turner <jenniferturner202@gmail.com>
2014-07-27 01:19:57 -0400

    Changed font and added jquery comment code.

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+/* $(function(){
+	$('.jerome-martin').show(1500);
+	$('.jerome-martin').fadeIn(2500);
+	$('.houseparty').hide();
+	$('.rock').hide();
+	$('.hair').hide();
+	$('.beverly-hills-90210').hide();
+	$('.super-mario').hide();
+
+
+});
+
+
+
+
+
+});
+
+*/
