0b227208e641f4e44971f4d20fe0a33b13a2287e
Jennifer Turner <jenniferturner202@gmail.com>
2014-07-27 01:27:31 -0400

    commented if/else statements

--- js/app.js
+++ js/app.js
@@ -13,7 +13,55 @@ $(document).ready(function(){
 });
 
 
+//if else statements for each answer
+for i in houseparty {
+	if (correct === "Sharane and Sidney") {
+	console.log("Right Answer!");
+	} else {
+	console.log("Sorry wrong answer")
+	}
+}
 
+
+for i in super-mario {
+	if (correct === "Princess Peach") {
+	console.log("Right Answer!");
+	} else {
+	console.log("Sorry wrong answer")
+	}
+}
+
+for i in jerome-martin {
+	if (correct === "Romey Rome") {
+	console.log("Right Answer!");
+	} else {
+	console.log("Sorry wrong answer")
+	}
+}
+
+for i in rock {
+	if (correct === "Seattle") {
+	console.log("Right Answer!");
+	} else {
+	console.log("Sorry wrong answer")
+	}
+}
+
+for i in hair {
+	if (correct === "Crimped Hair") {
+	console.log("Right Answer!");
+	} else {
+	console.log("Sorry wrong answer")
+	}
+}
+
+for i in beverly-hills-90210{
+	if (correct === "The Peach Pit") {
+	console.log("Right Answer!");
+	} else {
+	console.log("Sorry wrong answer")
+	}
+}
 
 
 });
